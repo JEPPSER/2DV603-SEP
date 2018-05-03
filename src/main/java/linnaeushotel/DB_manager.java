e56948e9ed34183bd72bd36f190158fa3890075b
Jonathan Berg (jb223pt) <jb223pt@student.lnu.se>
2018-05-03 03:05:51 +0200

    Update DB_manager.java

--- src/main/java/linnaeushotel/DB_manager.java
+++ src/main/java/linnaeushotel/DB_manager.java
@@ -1,13 +1,22 @@
 package linnaeushotel;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
 
 import linnaeushotel.guest.Guest;
+import linnaeushotel.reservation.Reservation;
 
 public class DB_manager{
 	
@@ -124,7 +133,54 @@ public class DB_manager{
 			}
 
 
-
+	public ArrayList<Guest >getGuests(){
+		ArrayList<Guest> guestList = new ArrayList<Guest>();
+	
+		try{
+			
+			uri= new MongoClientURI("mongodb+srv://test:123@cluster0-les1j.mongodb.net/test");
+			client= new MongoClient(uri);
+			
+			@SuppressWarnings("deprecation")
+			DB db= client.getDB("Hotel");
+			collection=db.getCollection("guest");
+		
+			DBCursor cursor =  collection.find();
+			
+			while(cursor.hasNext()){
+				
+				DBObject obj = cursor.next();
+				Guest g = new Guest();
+				g.setId((int)obj.get("_id"));
+				g.setFirstName((String)obj.get("firstName"));
+				g.setLastName((String)obj.get("lastName"));
+				g.setReservations((ArrayList<Reservation>)obj.get("Reservations"));
+				g.setAddress((String)obj.get("address"));
+				g.setPhone((String)obj.get("phone"));
+				g.setMobile((String)obj.get("mobile"));
+				g.setFax((String)obj.get("fax"));
+				g.setEmail((String)obj.get("email"));
+				g.setFavouriteRoom((String)obj.get("favouriteRoom"));
+				g.setSmoker((Boolean)obj.get("smoker"));
+				g.setBirthday((LocalDate)obj.get("birthday"));
+				g.setSpouse((String)obj.get("spose"));
+				g.setChildren((String)obj.get("children"));
+				g.setCompany((String)obj.get("company"));
+				
+				guestList.add(g);
+			}
+		
+			
+			client.close();
+			return guestList;
+			}catch(Exception e){
+				System.err.println(e);
+			}	
+		
+	
+		return null;
+		
+	}
 
 
 }
